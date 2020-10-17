6697a3f6a98885f22b2432c13b8ea9c5008c155c
Luiz Cardozo <luizfcardozo@gmail.com>
2020-10-17 11:02:21 -0300

    full landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
 import React from 'react';
+import { FiArrowRight } from 'react-icons/fi'
+import logoImg from './images/logo.svg'
+
 import './styles/global.css'
+import './styles/pages/landing.css'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={logoImg} alt="Happy"/>
+        <main>
+          <h1>Leve felicidade para o mundo</h1>
+          <p>Visite orfanatos e mudo o dia de muitas crianças</p>
+
+          <div className="location">
+            <strong>Florianópolis</strong>
+            <span>Santa Catarina</span>
+          </div>
+          <a href="#" className="enter-app">
+            <FiArrowRight size={26} color="rgba(0,0, 0, 0.6)"/>
+          </a>
+        </main>
+      </div>
     </div>
   );
 }
